954df3a6dcb5a1dc80bebd38b2bd62a10a85e75c
Gohara22 <gohara22@stac.edu>
2025-10-01 18:55:55 -0400

    Clean app ready for EB

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-mxwf690eb9mc7##qolq$8$vc%_=x$33qqb=h*mz1qw#z=*q(es
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =  ["my-polls-env.eba-mgrgc3yi.us-east-1.elasticbeanstalk.com"]
+ALLOWED_HOSTS =  ['my-polls-env.eba-mgrgc3yi.us-east-1.elasticbeanstalk.com','127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -116,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
